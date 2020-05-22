a6c000e3ff05876f20f047e753597fd6685023dc
Jesús G. Mijares M <jgmm81@gmail.com>
2020-05-21 20:28:05 -0400

    2020-05-21 C

---
+++ Insync_setup/config.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Insync config, prevent consume all Bandwidth and CPU Resources 
+# Rev 1.1 2020-05-21, jgmm81@gmail.com
+# Tested on: Ubuntu 20.04LTS with GNOME, 
+
+apt install -y trickle cpulimit sed
+
+ACTUAL_USER=$(who | awk '{print $1}')
+
+#In percent, default is 30
+CPU_MAX_USAGE=30
+
+# Default Bandwidth: Download 256 / Upload 128
+BW_MAX_DOWNLOAD=256
+BW_MAX_UPLOAD=128
+
+GNOME_DESKTOP_LAUNCHER=/usr/share/applications/insync.desktop
+GNOME_AUTOSTART_LAUCHER="/home/${ACTUAL_USER}/.config/autostart/insync.desktop"
+
+TRICKLE_CONFIG="s/^Exec=.*$/Exec=trickle -d ${BW_MAX_DOWNLOAD} -u ${BW_MAX_UPLOAD} insync start/"
+
+sed -i "${TRICKLE_CONFIG}" ${GNOME_DESKTOP_LAUNCHER}
+[[ -f "${GNOME_AUTOSTART_LAUCHER}" ]] && sed -i "${TRICKLE_CONFIG}" ${GNOME_AUTOSTART_LAUCHER}
+
+###CPU Limit -> make service autosart ####
+
+INSYNC_PATH=/opt/cpuusage/insync
+BASH_FILE="${INSYNC_PATH}/insync_cpu_usage.sh"
+SERVICE_FILE="${INSYNC_PATH}/insync_cpu_usage.service"
+
+mkdir -p "${INSYNC_PATH}"
+
+cat > "${BASH_FILE}" << EOL
+#!/bin/bash
+## CPU max usage for insync
+
+LOG_FILE=${INSYNC_PATH}/cpulimit_errors.log
+
+echo "---------------------------" | tee >> "\${LOG_FILE}"
+date | tee >> "\${LOG_FILE}"
+
+cpulimit -e insync -l ${CPU_MAX_USAGE} -b -q 2>&1 | tee >> "\${LOG_FILE}"
+
+echo "---------------------------" | tee >> "\${LOG_FILE}"
+EOL
+
+chmod 755 "${BASH_FILE}"
+
+cat > "${SERVICE_FILE}" << EOL
+[Unit]
+Description=CPU Limit for insync
+
+[Service]
+Type=oneshot
+ExecStart=/bin/bash ${BASH_FILE}
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+chmod 755 "${SERVICE_FILE}"
+
+[[ ! -f /etc/systemd/system/insync_cpu_usage.service ]] && ln -s ${SERVICE_FILE} /etc/systemd/system/insync_cpu_usage.service
+
+systemctl daemon-reload
+###systemctl start insync_cpu_usage.service
+systemctl enable insync_cpu_usage.service
+
---
+++ cpu/hp 8470p/config.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Set services to automatic start cpupower settings
+# Rev 1.4.1 2020-05-21, jgmm81@gmail.com
+# Tested on: Ubuntu 20.04LTS, 
+
+CPUPOWER_PATH=/opt/cpuondemand
+BASH_FILE="${CPUPOWER_PATH}/run_cpuondemand.sh"
+SERVICE_FILE="${CPUPOWER_PATH}/cpupower.service"
+
+#Set seconds to sleep according machine speed for wait to OS load
+WAIT_SECONDS=25
+
+#Default 2.20
+MAX_CPU_GHZ=2.20
+
+mkdir -p "${CPUPOWER_PATH}"
+
+cat > "${BASH_FILE}" << EOL
+#!/bin/bash
+## for the CPU Governor and keep it running
+
+LOG_FILE="${CPUPOWER_PATH}/startup.log"
+
+echo "---------------------------" | tee >> "\${LOG_FILE}"
+date | tee >> "\${LOG_FILE}"
+
+#Set seconds to sleep according machine speed for wait to OS load
+sleep ${WAIT_SECONDS}
+
+apt-get install -y linux-tools-common linux-tools-\$(uname -r) 2>&1 | tee >> "\${LOG_FILE}"
+
+/usr/bin/cpupower --cpu all frequency-set --max ${MAX_CPU_GHZ}GHz 2>&1 | tee >> "\${LOG_FILE}"
+
+date | tee >> "\${LOG_FILE}"
+echo "---------------------------" | tee >> "\${LOG_FILE}"
+EOL
+
+chmod 755 "${BASH_FILE}"
+
+cat > "${SERVICE_FILE}" << EOL
+[Unit]
+Description=CPU ondemand
+
+[Service]
+Type=oneshot
+ExecStart=/bin/bash ${BASH_FILE}
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+chmod 755 "${SERVICE_FILE}"
+
+[[ ! -f /etc/systemd/system/cpupower.service ]] && ln -s ${SERVICE_FILE} /etc/systemd/system/cpupower.service
+
+systemctl daemon-reload
+systemctl start cpupower.service
+systemctl enable cpupower.service
